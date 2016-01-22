88002d8ac65f0279b236c0cb7f82ef899064b8d2
ogaya <wadesu012@gmail.com>
2016-01-22 23:53:12 +0900

    Edit Cellにセル結合追加

--- src/edit-cell/index.tsx
+++ src/edit-cell/index.tsx
@@ -5,29 +5,52 @@ import {
     CellPoint,
     CellRange,
     TEXT_ALIGN,
-    VERTICAL_ALIGN
+    VERTICAL_ALIGN,
+    BORDER_POSITION
 } from "react-gridview";
+
+function createSheet() {
+    // Sheet is an Immutable Record
+    let sheet = Sheet.create();
+
+    // edit cells
+    const range = CellRange.create(CellPoint.create(3, 3), CellPoint.create(6, 12));
+    sheet = sheet.editCells(range, (cell, cellPoint) => {
+        return cell
+            .setValue(cellPoint.columnNo + cellPoint.rowNo)
+            .setTextAlign(TEXT_ALIGN.CENTER);
+    });
+
+    // merge Cells
+    sheet = sheet
+        .mergeRange(CellRange.create(3, 2, 4, 2))
+        .mergeRange(CellRange.create(5, 2, 6, 2))
+        .mergeRange(CellRange.create(2, 3, 2, 12))
+        .editCell(CellPoint.create(3, 2), (cell) => {
+            return cell
+                .setValue("merge1")
+                .setBackground("#B3E5FC")
+                .setTextAlign(TEXT_ALIGN.CENTER);
+        })
+        .editCell(CellPoint.create(5, 2), (cell) => {
+            return cell
+                .setValue("merge2")
+                .setBackground("#A5D6A7")
+                .setTextAlign(TEXT_ALIGN.CENTER);
+        })
+        .editCell(CellPoint.create(2, 3), (cell) => {
+            return cell
+                .setValue("merge3")
+                .setBackground("#FFCC80")
+                .setTextAlign(TEXT_ALIGN.CENTER);
+        });
+
+    return sheet;
+}
 
 export class EditCell extends React.Component<{}, {}>{
     render() {
-        let sheet = Sheet.create()
-            .editCell(CellPoint.create(2, 3), (cell) => {
-                return cell
-                    .setValue(5)
-                    .setTextAlign(TEXT_ALIGN.CENTER);
-            })
-            .editCell(CellPoint.create(3, 3), (cell) => {
-                return cell
-                    .setValue(6)
-                    .setTextAlign(TEXT_ALIGN.CENTER);
-            });
-        const range = CellRange.create(CellPoint.create(2, 4), CellPoint.create(3, 12));
-        sheet = sheet.editCells(range, (cell, cellPoint) => {
-                return cell
-                    .setValue(cellPoint.columnNo + cellPoint.rowNo)
-                    .setTextAlign(TEXT_ALIGN.CENTER);
-            });
-
+        const sheet = createSheet();
         return (
             <GridView className="edit-cell" sheet={sheet} />
         );
